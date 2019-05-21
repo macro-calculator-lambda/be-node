08e0eb7188343e21b66339d16b16a18cbc2ebac9
spinon187 <spinon187@gmail.com>
2019-05-21 10:55:01 -0500

    first push

---
+++ data/migrations/20190521102106_users.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('users', function(tbl){
+        tbl.increments('id');
+        tbl.string('username')
+            .unique()
+            .notNullable();
+        tbl.string('password')
+            .notNullable();
+        tbl.string('gender')
+            .notNullable();
+        tbl.integer('age')
+            .notNullable();
+        tbl.string('height')
+            .notNullable();
+        tbl.integer('weight')
+            .notNullable();
+        tbl.string('goal')
+            .notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
